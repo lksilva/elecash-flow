4428f8c855043267a3a04448cddaab5ec6b78f57
Lucas Silva <lucas_s.s@hotmail.com>
2017-07-24 16:37:42 -0300

    Creating form

--- app/components/Register/Register.js
+++ app/components/Register/Register.js
@@ -2,35 +2,35 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import styles from './Register.css';
-import { blue500 } from 'material-ui/styles/colors';
+import DatePicker from 'material-ui/DatePicker';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
-const inlinestyles = {
-  floatingLabelFocusStyle: {
-    color: blue500,
-  },
-  underlineStyle: {
-    borderColor: '#789fdc',
-  },
-  floatingLabelStyle: {
-    color: '#789fdc',
-  },
-};
-
 class Register extends Component {
+
+  formatDate(date) {
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  }
+
   render() {
     return (
       <div>
-        <h1 className={styles.center}>ORDEM DE SERVIÇO</h1>
-        <form>
-          <TextField
-            floatingLabelText="Float text"
-            underlineStyle={inlinestyles.underlineStyle}
-            floatingLabelStyle={inlinestyles.floatingLabelStyle}
-            floatingLabelFocusStyle={inlinestyles.floatingLabelFocusStyle}
-          />
+        <h3 className={styles.center}>ORDEM DE SERVIÇO</h3>
+        <form className={styles.form}>
+          <div className={styles.card}>
+            <TextField
+              floatingLabelText="TSO"
+            />
+            <TextField
+              floatingLabelText="Número do Cliente"
+            />
+            <DatePicker
+              hintText="Portrait Inline Dialog"
+              container="inline"
+              formatDate={this.formatDate}
+            />
+          </div>
         </form>
       </div>
     );
--- app/index.js
+++ app/index.js
@@ -7,11 +7,20 @@ import { configureStore, history } from './store/configureStore';
 import './app.global.css';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { blue500, darkBlack } from 'material-ui/styles/colors';
 
 const store = configureStore();
 
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: darkBlack,
+    disabledColor: '#789fdc'
+  },
+  fontFamily: 'Roboto, sans-serif',
+});
+
 render(
-  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <AppContainer>
       <Root store={store} history={history} />
     </AppContainer>
@@ -23,7 +32,7 @@ if (module.hot) {
   module.hot.accept('./containers/Root', () => {
     const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
     render(
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <AppContainer>
           <NextRoot store={store} history={history} />
         </AppContainer>
