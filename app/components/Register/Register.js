a8ebad23ab900f6f6816a969745f418ab14c3b5e
Lucas Silva <lucas_s.s@hotmail.com>
2017-07-25 11:21:18 -0300

    Apply mask on inputs

--- app/components/Register/Register.js
+++ app/components/Register/Register.js
@@ -4,6 +4,7 @@ import TextField from 'material-ui/TextField';
 import styles from './Register.css';
 import DatePicker from 'material-ui/DatePicker';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import InputMask from 'react-input-mask';
 
 injectTapEventPlugin();
 
@@ -13,7 +14,10 @@ const customStyle = {
   },
   fullRow: {
     flex: '1 100%',
-    margin: '0 8px 0 8px'
+    margin: '0 8px 0 8px',
+  },
+  upperCase: {
+    textTransform: 'uppercase'
   }
 };
 
@@ -31,9 +35,11 @@ class Register extends Component {
           <div className={styles.card}>
             <TextField
               floatingLabelText="TSO"
+              type="number"
             />
             <TextField
               floatingLabelText="NÚMERO DO CLIENTE"
+              type="number"
             />
             <DatePicker
               floatingLabelText="DATA"
@@ -44,51 +50,62 @@ class Register extends Component {
             <TextField
               style={customStyle.fullRow}
               floatingLabelText="NOME"
+              inputStyle={customStyle.upperCase}
             />
             <TextField
               style={customStyle.fullRow}
               floatingLabelText="PAI"
+              inputStyle={customStyle.upperCase}
             />
             <TextField
               style={customStyle.fullRow}
               floatingLabelText="MÃE"
+              inputStyle={customStyle.upperCase}
             />
             <TextField
               floatingLabelText="RG"
+              type="number"
             />
             <TextField
               floatingLabelText="ORG EXP"
+              inputStyle={customStyle.upperCase}
             />
-            <TextField
-              floatingLabelText="CPF"
-            />
-            <TextField
-              floatingLabelText="DATA DE NASCIMENTO"
-            />
+            <TextField floatingLabelText="CPF">
+              <InputMask mask="999.999.999-99" />
+            </TextField>
+            <TextField floatingLabelText="DATA DE NASCIMENTO">
+              <InputMask mask="99/99/9999" />
+            </TextField>
             <TextField
               floatingLabelText="ENDEREÇO"
+              inputStyle={customStyle.upperCase}
             />
             <TextField
               floatingLabelText="BAIRRO"
+              inputStyle={customStyle.upperCase}
             />
             <TextField
               floatingLabelText="CIDADE"
+              inputStyle={customStyle.upperCase}
             />
             <TextField
               floatingLabelText="UF"
+              maxLength="2"
+              inputStyle={customStyle.upperCase}
             />
-            <TextField
-              floatingLabelText="CEP"
-            />
-            <TextField
-              floatingLabelText="TELEFONE RESIDENCIAL"
-            />
+            <TextField floatingLabelText="CEP">
+              <InputMask mask="99999-9999" />
+            </TextField>
+            <TextField floatingLabelText="TELEFONE RESIDENCIAL">
+              <InputMask mask="(99) 9999-9999" />
+            </TextField>
             <TextField
               floatingLabelText="REFERÊNCIAL"
+              inputStyle={customStyle.upperCase}
             />
-            <TextField
-              floatingLabelText="CELULAR"
-            />
+            <TextField floatingLabelText="CELULAR">
+              <InputMask mask="(99) 999999999" />
+            </TextField>
           </div>
         </form>
       </div>
